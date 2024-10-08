9ba419ceb959f13c9c60bf32b2bff8b92e7687cf
Jessica Story <nyngdpyqx@proton.me>
2024-10-08 04:46:18 +0000

    format

--- functional/hello-world-scala/src/main/scala/HelloWorldServer.scala
+++ functional/hello-world-scala/src/main/scala/HelloWorldServer.scala
@@ -7,35 +7,32 @@ import scala.io.StdIn
 import io.circe.parser._
 import scala.util.Using
 
+
 object HelloWorldServer {
-  def main(args: Array[String]): Unit = {
-    // Create an ActorSystem
     implicit val system = ActorSystem(Behaviors.empty, "helloWorldSystem")
     implicit val executionContext = system.executionContext
 
-    def insertionSort[T](list: List[T])(implicit ord: Ordering[T]): List[T] = {
-  list match {
-    case Nil => Nil // Base case: an empty list is already sorted
-    case head :: tail =>
-      insert(head, insertionSort(tail)) // Insert the head into the sorted tail
+     def main(args: Array[String]): Unit = {
+    def insertionSort(list: List[String]): List[String] = {
+    list match {
+      case Nil => Nil
+      case head :: tail =>
+      insert(head, insertionSort(tail))
+    }
   }
-}
 
-def insert[T](element: T, sortedList: List[T])(implicit ord: Ordering[T]): List[T] = {
-  sortedList match {
-    case Nil => List(element) // If the sorted list is empty, just add the element
-    case head :: tail =>
-      if (ord.lt(element, head)) element :: sortedList // Insert before the head if smaller
-      else head :: insert(element, tail) // Recursively insert into the tail
+  def insert(element: String, sortedList: List[String]): List[String] = {
+    sortedList match {
+      case Nil => List(element)
+      case head :: tail =>
+        if (element < head) element :: sortedList
+        else head :: insert(element, tail)
+    }
   }
-}
 
-
-    // Specify the JSON file path in the resources folder
-    val jsonFilePath = getClass.getResourceAsStream("/list.json")
-
-    // Read the JSON file into a List[String]
-    val stringList = Using(jsonFilePath) { stream =>
+    val fileName = getClass.getResourceAsStream("/list.json")
+    //reading in file
+    val stringList = Using(fileName) { stream =>
       val jsonString = Source.fromInputStream(stream).getLines().mkString("\n")
 
       // Parse the JSON string into a List[String]
@@ -62,9 +59,22 @@ def insert[T](element: T, sortedList: List[T])(implicit ord: Ordering[T]): List[
           complete(s"Hello, $person!")
         }
       } ~
-      path("listOfStrings") {
+      path("sortedStringList") {
         get {
-          complete(s"List 1:\n${stringList.sorted.mkString(", ")}\n\nList 2:\n${stringList.sortWith((a, b) => a.toLowerCase < b.toLowerCase)}\n\n${insertionSort(stringList)}")
+         complete(
+                  s"""Unsorted List:
+                  |${stringList.mkString(", ")}
+                  |
+                  |List Sorted By Built-In Sort:
+                  |${stringList.sorted.mkString(", ")}
+                  |
+                  |List Sorted by Sort With:
+                  |${stringList.sortWith((a, b) => a.toLowerCase < b.toLowerCase).mkString(", ")}
+                  |
+                  |List Sorted By Insertion Sort:
+                  |${insertionSort(stringList).mkString(", ")}""".stripMargin
+                  )
+        
         }
       }
 
