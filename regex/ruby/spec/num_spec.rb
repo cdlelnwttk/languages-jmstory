6b2bd0309a2a32a256b45eb045ea04602ded0a36
Jessica Story <nyngdpyqx@proton.me>
2024-10-26 01:07:22 +0000

    added ' in all of them

--- regex/ruby/spec/num_spec.rb
+++ regex/ruby/spec/num_spec.rb
@@ -29,11 +29,11 @@ end
 
 RSpec.describe 'Regular Expression for octal literal' do
   let(:octal_digit) {  /[0-7]/ }
+  let (:octal) {/'?#{octal_digit}+/}
+  let(:pattern) {/^0#{octal}*$/}
 
-  let(:pattern) {/^0#{octal_digit}*$/}
-
-  let(:should_pass) { ["0", "077772", "0111", "01234567"]}
-  let(:should_fail) {["1", "08", "9"]}
+  let(:should_pass) { ["0", "077772", "0111", "01234567", "0'1'1'1", "0100'100"]}
+  let(:should_fail) {["1", "08", "9", "0'''1", "'000", "0'"]}
   describe 'should pass' do
     it 'matches all expected strings' do
       should_pass.each do |str|
@@ -53,10 +53,10 @@ end
 RSpec.describe 'Regular Expression for hex literal' do
   let(:hex_digit) {  /[0-9]|[A-F]|[a-f]/ }
   let(:hex_start) {/0[xX]/}
-
-  let (:pattern) {/^#{hex_start}#{hex_digit}+$/}
-  let (:should_pass) {["0Xfffff", "0xfffff", "0x123abc", "0x1", "0xabcdef"]}
-  let (:should_fail) {["fffff", "0x", "0X", "12345", "abcdef", "x133f", "0xzzzzz"]}
+  let (:hex) {/'?#{hex_digit}+/}
+  let (:pattern) {/^#{hex_start}#{hex_digit}#{hex}*$/}
+  let (:should_pass) {["0Xfffff", "0xfffff", "0x123ab'c", "0x1", "0xabcdef", "0x1'1'1"]}
+  let (:should_fail) {["fffff", "0x", "0X", "12345", "abcdef", "x133f", "0xzzzzz", "0'x11", "0x'", "0xffff'", "0xf'''f"]}
 
   describe 'should pass' do
     it 'matches all expected strings' do
@@ -77,11 +77,11 @@ end
 RSpec.describe 'Regular Expression for binary literal' do
   let(:binary_digit) {  /[0-1]/ }
   let (:binary_start) {/0[bB]/}
+  let (:binary) {/'?#{binary_digit}+/}
+  let(:pattern) {/^#{binary_start}#{binary_digit}#{binary}*$/}
 
-  let(:pattern) {/^#{binary_start}#{binary_digit}+$/}
-
-  let(:should_pass) { ["0B10101001", "0b1", "0B0", "0B000000000000000", "0b111111111111111111111111", "0b1010100101001"]}
-  let(:should_fail) {["4", "0b", "0B", "10101001", "0b2", "0B34"]}
+  let(:should_pass) { ["0B10101001", "0b1", "0B0", "0B0'0'0000'000000000", "0b1'11111111111111111111111", "0b1010100101001"]}
+  let(:should_fail) {["4", "0b", "0B", "10101001", "0b2", "0B34", "0b'1", "0b1''1"]}
   describe 'should pass' do
     it 'matches all expected strings' do
       should_pass.each do |str|
---
+++ regex/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+    int binary = 0B1'1;
+    int binary = 0b1;
+
+    int hex = 0x1'11'1;
+    int oct = 0'1'1;
+    return 0;
+}
